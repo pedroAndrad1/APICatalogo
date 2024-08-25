da2edf231c3a0ed78bdd76e4def63b1b5bbf67b9
Pedro de Andrade <dev.pedroandrade@gmail.com>
2024-08-25 12:46:22 -0300

    feat: Fazendo Validações na via classe

--- APICatalogo.Application/Abstractions/ProdutoCommand.cs
+++ APICatalogo.Application/Abstractions/ProdutoCommand.cs
@@ -5,12 +5,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace APICatalogo.Application.Abstractions
 {
-    public class ProdutoCommand : IRequest<ProdutoModel>
+    public class ProdutoCommand : IRequest<ProdutoModel>, IValidatableObject
     {
         public Guid? Id { get; init; }
         [Required]
         [StringLength(80)]
-        [PrimeiraLetraMaiuscula]
+        //[PrimeiraLetraMaiuscula]
         public string Nome { get; init; } = string.Empty;
         [Required]
         [StringLength(300)]
@@ -22,9 +22,25 @@ namespace APICatalogo.Application.Abstractions
         [StringLength(300)]
         public string ImageUrl { get; init; } = string.Empty;
         [Required]
-        [Range(0, int.MaxValue, ErrorMessage = "Valor mínimo de zero itens em estoque.")]
+        //[Range(0, int.MaxValue, ErrorMessage = "Valor mínimo de zero itens em estoque.")]
         public float Estoque { get; init; }
         [Required]
         public Guid CategoriaId { get; init; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            string primeiraLetra = this.Nome.ToString()[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new ValidationResult("A primeira letra deve ser maiúscula.", new[] { nameof(this.Nome) });
+
+            }
+
+            if(this.Estoque < 0)
+            {
+                yield return new ValidationResult("Valor mínimo de zero itens em estoque.", new[] { nameof(this.Estoque) });
+            }
+
+        }
     }
 }
