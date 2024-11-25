c31e7df485a291b84c19995d1b820410ffe6300a
Pedro de Andrade <dev.pedroandrade@gmail.com>
2024-11-24 22:20:34 -0300

    feat: Serviço de Token

---
+++ APICatalogo.Application/Services/TokenService.cs
@@ -0,0 +1,80 @@
+﻿using APICatalogo.Domain.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace APICatalogo.Application.Services
+{
+    public class TokenService : ITokenService
+    {
+        public JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration configuration)
+        {
+            // Obtendo chave secreta
+            var privateKey = configuration.GetSection("JWT").GetValue<string>("SecretKey") ??
+                 throw new InvalidOperationException("Erro ao obter private key.");
+            // Parseando para um array de Bytes 
+            var encodedPrivateKey = Encoding.UTF8.GetBytes(privateKey); 
+            // Criando uma asssinatura encriptada
+            var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(encodedPrivateKey), SecurityAlgorithms.HmacSha256Signature);
+            // Descrevendo Token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(configuration.GetSection("JWT").GetValue<double>("TokenValidityInMinures")),
+                Audience = configuration.GetSection("JWT").GetValue<string>("ValidAudiences"),
+                Issuer = configuration.GetSection("JWT").GetValue<string>("ValidIssuer"),
+                SigningCredentials = signingCredentials
+            };
+            // Gerando Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+
+            return token;
+        }
+
+        public string GenerateRefreshToken()
+        {
+            var secureRandomBytes = new byte[128];
+            var randomNumberGenerator = RandomNumberGenerator.Create();
+            randomNumberGenerator.GetBytes(secureRandomBytes);
+            
+            var refreshToken = Convert.ToBase64String(secureRandomBytes);
+            return refreshToken;
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string expiredToken, IConfiguration configuration)
+        {
+            var privateKey = configuration.GetSection("JWT").GetValue<string>("SecretKey") ??
+                throw new InvalidOperationException("Erro ao obter private key.");
+            var encodedPrivateKey = Encoding.UTF8.GetBytes(privateKey);
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(encodedPrivateKey),
+                ValidateLifetime = false,
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(expiredToken, tokenValidationParameters, out SecurityToken securityToken);
+
+            if (
+                securityToken is not JwtSecurityToken jwtSecurityToken ||
+                !jwtSecurityToken.Header.Alg.Equals(
+                    SecurityAlgorithms.HmacSha256,
+                    StringComparison.InvariantCultureIgnoreCase
+                )
+            )
+            {
+                throw new SecurityTokenException();
+            }
+
+            return principal;
+           
+        }
+    }
+}
---
+++ ApiCatalogo.Domain/Services/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace APICatalogo.Domain.Services
+{
+    public interface ITokenService
+    {
+        JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration configuration);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string expiredToken, IConfiguration configuration);
+    }
+}
